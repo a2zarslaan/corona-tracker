c492376a2cca1a1fb4c198095bb6efdc48c5dc19
Amar Arslaan <a2zarslaan@hotmail.com>
2020-05-09 16:46:45 +0100

    Updates

---
+++ precache-manifest.509a4079e605d5e3387021642edc091e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd21a9fbc861055ff1e3c110cc4ba20f",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "34e3f89df574b024ae99",
+    "url": "/corona-tracker/static/css/main.82178bd2.chunk.css"
+  },
+  {
+    "revision": "7cf2667faab956c7f09c",
+    "url": "/corona-tracker/static/js/2.32598d73.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.32598d73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34e3f89df574b024ae99",
+    "url": "/corona-tracker/static/js/main.887f1ab0.chunk.js"
+  },
+  {
+    "revision": "8749c467ffc39be70ee0",
+    "url": "/corona-tracker/static/js/runtime-main.67a5e37e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/virus.d7265326.png"
+  }
+]);
